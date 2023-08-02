3040bb17a9fa5bbef548b0feefa089ed8ef44952
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-02 17:10:02 -0300

    cors

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 // server.js
 import express from "express";
 import mercadopago from "mercadopago";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  next();
-});
+app.use(cors())
 
 
 mercadopago.configure({
