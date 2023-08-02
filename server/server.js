516ccb7294663388b3af2a7cb25c469af43fcb4e
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-01 21:53:51 -0300

    permisos a las url

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ import cors from "cors";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://64c9a75ef014710008f11e8b--cantinero.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(
   cors({
