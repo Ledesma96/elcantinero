edc9ffac11634a06f869c20f4984a266f6ebfb40
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-01 22:10:41 -0300

    a ver ahora

--- server/server.js
+++ server/server.js
@@ -14,11 +14,16 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "https://cantinero.netlify.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  next();
+  // Responder a la solicitud OPTIONS para las preflight requests
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 mercadopago.configure({
